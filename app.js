71564d5f56267d2724a17de5b782a1d2be404cf6
catiecook <catiecook888@gmail.com>
2016-11-30 14:02:56 -0700

    fixing cors issues

--- app.js
+++ app.js
@@ -23,6 +23,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/users', users);
 
 // catch 404 and forward to error handler
